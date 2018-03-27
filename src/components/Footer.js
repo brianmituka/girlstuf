db10663b525f616df8f19f50aa34ec28136b89d5
brianmituka <mitukab@outlook.com>
2018-03-27 07:59:12 +0300

    Added the footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,11 @@ const Footer = (props) => {
             <p className="footer-links">
             
                 <span>  /  </span>
-                <a href="mailto:info@girlstuff.co.ke" target="_blank">info@girlstuff.co.ke</a>
+                <a href="mailto:info@girlstuff.co.ke" target="_blank" rel="noopener noreferrer">info@girlstuff.co.ke</a>
                 <span>  /  </span>
-                <a href="https://twitter.com/Girlstuff_ke" target="_blank">@Girlstuff_ke</a>
+                <a href="https://twitter.com/Girlstuff_ke" target="_blank" rel="noopener noreferrer">@Girlstuff_ke</a>
                 <span>  /  </span>
-                <a href="" target="_blank">0715600582</a>
+                <a href="" target="_blank" rel="noopener noreferrer">0715600582</a>
             </p>
             <p>&copy; 2018 <strong>Girlstuff</strong> - Powered by Nouveta Limited</p>
         </footer>
